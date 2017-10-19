be1567594acf5ceeba8cfd279cd37bfcc4950fd4
etermaw <etermaw@gmail.com>
2017-10-19 21:17:43 +0200

    moved all core functionality into separate module

--- GameBoyEmu/main.cpp
+++ GameBoyEmu/main.cpp
@@ -1,119 +1,77 @@
 #include "stdafx.h"
-#include "cpu.h"
-#include "mmu.h"
-#include "ram.h"
-#include "cartrige.h"
-#include "interrupts.h"
-#include "gpu.h"
-#include "timer.h"
-#include "joypad.h"
-#include "apu.h"
-#include "debugger.h"
+#include "Core.h"
 #include "audio_postprocess.h"
 
-struct TestReader final : public IMemory
-{
-	u8 a;
-
-	public:
-		TestReader() : a() {}
-
-		u8 read_byte(u16 adress, u32 unused) override
-		{
-			return 0;
-			//return 0xFF;
-		}
-
-		void write_byte(u16 adress, u8 val, u32 unused) override
-		{
-			if (adress == 0xFF01)
-				a = val;
-
-			if (adress == 0xFF02 && val == 0x81)
-				printf("%c", a);
-		}
-};
-
-struct SpeedSwitch : public IMemory
-{
-	bool double_speed = false;
-	bool switch_speed = false;
-
-	u8 read_byte(u16 adress, u32 unused) override
-	{
-		if (adress == 0xFF4D)
-		{
-			auto ret = change_bit(0xFF, double_speed, 7);
-			return change_bit(ret, switch_speed, 0);
-		}
-
-		else
-			return 0xFF;
-	}
-
-	void write_byte(u16 adress, u8 val, u32 key) override
-	{
-		if (adress == 0xFF4D)
-		{
-			if (key == 0xFFFFFFFF && val == 0xFF)
-			{
-				switch_speed = false;
-				double_speed = !double_speed;
-			}
-
-			else
-				switch_speed = check_bit(val, 0);
-		}
-
-		//else ignore
-	}
-
-	void serialize(std::ostream& stream)
-	{
-		stream << switch_speed << double_speed;
-	}
-
-	void deserialize(std::istream& stream)
-	{
-		stream >> switch_speed >> double_speed;
-	}
-};
 
 int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 
-	Interrupts ints;
-	Timer timer(ints);
-	MMU mmu;
-	Ram ram;
-	Cartrige cart;
-	TestReader tr; //testing
-	Joypad joypad;
-	CPU cpu(mmu);
-	Gpu gpu(ints);
-	APU apu;
-	SpeedSwitch speed;
-
-	Audio audio_post;
-
-	Debugger debugger;
-	debugger.attach_mmu(make_function(&MMU::read_byte, &mmu), make_function(&MMU::write_byte, &mmu));
-	debugger.attach_gpu(gpu.get_debug_func());
-
-	cpu.attach_debugger(debugger.get_cpu());
-	mmu.attach_debug_callback(make_function(&Debugger::check_memory_access, &debugger));
-
 	SDL_Window* window = SDL_CreateWindow("Test",
-											SDL_WINDOWPOS_UNDEFINED,
-											SDL_WINDOWPOS_UNDEFINED,
-											160 * 3,
-											144 * 3,
-											0);
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		160 * 3,
+		144 * 3,
+		0);
 
 	SDL_Renderer* rend = SDL_CreateRenderer(window, -1, 0);
 	SDL_Texture* tex = SDL_CreateTexture(rend, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 160, 144);
-	SDL_Event ev;
+
+	auto vblank_handler = [&](const u32* frame_buffer) 
+	{
+		void* pixels = nullptr;
+		int pitch = 0;
+
+		SDL_LockTexture(tex, NULL, &pixels, &pitch);
+		std::memcpy(pixels, frame_buffer, sizeof(u32) * 160 * 144);
+		SDL_UnlockTexture(tex);
+
+		SDL_RenderClear(rend);
+		SDL_RenderCopy(rend, tex, NULL, NULL);
+		SDL_RenderPresent(rend); 
+	};
+
+	auto input_handler = [](Joypad& input)
+	{
+		SDL_Event ev;
+		static const u32 key_map[8] = { SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN, SDLK_a, SDLK_b, SDLK_RETURN, SDLK_s };
+
+		while (SDL_PollEvent(&ev))
+		{
+			if (ev.type == SDL_KEYDOWN || ev.type == SDL_KEYUP)
+			{
+				auto key_code = std::find(std::begin(key_map), std::end(key_map), ev.key.keysym.sym);
+
+				if (key_code != std::end(key_map))
+				{
+					auto index = std::distance(std::begin(key_map), key_code);
+
+					if (ev.type == SDL_KEYDOWN)
+						input.push_key(static_cast<KEYS>(index));
+
+					else
+						input.release_key(static_cast<KEYS>(index));
+				}
+			}
+
+			else if (ev.type == SDL_QUIT)
+				return false;
+		}
+
+		return true;
+	};
+
+	Core emu_core;
+	Audio audio_post;
+
+	external_callbacks endpoints;
+
+	endpoints.audio_control = make_function(&Audio::dummy, &audio_post);
+	endpoints.swap_sample_buffer = make_function(&Audio::swap_buffers, &audio_post);
+	endpoints.draw_frame = make_function(&decltype(vblank_handler)::operator(), &vblank_handler);
+	endpoints.update_input = make_function(&decltype(input_handler)::operator(), &input_handler);
+
+	emu_core.attach_callbacks(endpoints);
 
 	std::string file_name;
 
@@ -122,7 +80,7 @@ int main(int argc, char *argv[])
 		std::cout << "Insert cartrige path:\n";
 		std::cin >> file_name;
 
-		if (cart.load_cartrige(file_name))
+		if (emu_core.load_cartrige(file_name))
 		{
 			std::cout << "Cartrige loaded!\n";
 			break;
@@ -131,100 +89,7 @@ int main(int argc, char *argv[])
 		std::cout << "Failed to load cartrige!\n";
 	}
 
-	mmu.register_chunk(0, 0x7FFF, cart.get_memory_controller());
-	mmu.register_chunk(0x8000, 0x9FFF, &gpu); //vram
-	mmu.register_chunk(0xA000, 0xBFFF, cart.get_memory_controller());
-	mmu.register_chunk(0xC000, 0xFDFF, &ram);
-	mmu.register_chunk(0xFE00, 0xFE9F, &gpu); //oam tables
-	mmu.register_chunk(0xFF00, 0xFF00, &joypad);//input keys register
-	mmu.register_chunk(0xFF01, 0xFF02, &tr); //TEST READER!!!!
-	mmu.register_chunk(0xFF04, 0xFF07, &timer);//timer controls
-	mmu.register_chunk(0xFF0F, 0xFF0F, &ints);//interrupts flags
-	mmu.register_chunk(0xFF10, 0xFF3F, &apu); //APU registers + wave RAM 
-	mmu.register_chunk(0xFF40, 0xFF4B, &gpu); //gpu control regs
-	mmu.register_chunk(0xFF4D, 0xFF4D, &speed); //CPU speed switch (CGB)
-	mmu.register_chunk(0xFF4F, 0xFF4F, &gpu); //gpu vram bank reg (CGB)
-	mmu.register_chunk(0xFF51, 0xFF55, &gpu); //gpu HDMA/GDMA regs (CGB)
-	mmu.register_chunk(0xFF68, 0xFF6B, &gpu); //gpu color BGP/OBP regs (CGB)
-	mmu.register_chunk(0xFF70, 0xFF70, &ram); //ram bank register (CGB)
-	mmu.register_chunk(0xFF80, 0xFFFE, &ram); //high ram
-	mmu.register_chunk(0xFFFF, 0xFFFF, &ints); //interrupts
-
-	gpu.attach_dma_ptrs(cart.get_dma_controller(), &ram);
-	apu.attach_endpoints(make_function(&Audio::swap_buffers, &audio_post), make_function(&Audio::dummy, &audio_post));
-
-	bool enable_cgb = cart.is_cgb_ready();
-	cpu.enable_cgb_mode(enable_cgb);
-	gpu.enable_cgb_mode(enable_cgb);
-	ram.enable_cgb_mode(enable_cgb);
-
-	bool spin = true;
-	const u32 key_map[8] = { SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN, SDLK_a, SDLK_b, SDLK_RETURN, SDLK_s };
-
-	while (spin)
-	{
-		while (SDL_PollEvent(&ev))
-		{
-			if (ev.type == SDL_KEYDOWN || ev.type == SDL_KEYUP)
-			{
-				auto key_code = std::find(std::begin(key_map), std::end(key_map), ev.key.keysym.sym);
-				
-				if (key_code != std::end(key_map))
-				{
-					auto index = std::distance(std::begin(key_map), key_code);
-
-					if (ev.type == SDL_KEYDOWN)
-						joypad.push_key(static_cast<KEYS>(index));
-
-					else
-						joypad.release_key(static_cast<KEYS>(index));
-				}
-			}
-			
-			else if (ev.type == SDL_QUIT)
-				spin = false;
-		}
-
-		while (!gpu.is_entering_vblank()) //TODO: if someone turn off lcd, this loop may spin forever
-		{
-			u32 sync_cycles = 0;
-
-			if (ints.is_any_raised())
-			{
-				cpu.unhalt();
-
-				if (cpu.is_interrupt_enabled())
-					sync_cycles = cpu.handle_interrupt(ints.get_first_raised());
-			}
-
-			debugger.step();
-
-			sync_cycles += cpu.step(sync_cycles);
-			sync_cycles += gpu.step(sync_cycles);
-			apu.step(sync_cycles);
-			timer.step(sync_cycles);
-
-			gpu.set_speed(speed.double_speed);
-			apu.set_speed(speed.double_speed);
-
-			//audio_sync_callback();
-		}
-
-		auto ptr = gpu.get_frame_buffer();
-		void* pixels = nullptr;
-		int pitch = 0;
-
-		SDL_LockTexture(tex, NULL, &pixels, &pitch);
-		std::memcpy(pixels, ptr, sizeof(u32) * 160 * 144);
-		SDL_UnlockTexture(tex);
-
-		SDL_RenderClear(rend);
-		SDL_RenderCopy(rend, tex, NULL, NULL);
-		SDL_RenderPresent(rend);
-
-		gpu.clear_frame_buffer();
-		debugger.after_vblank();
-	}
+	emu_core.run();
 
 	SDL_DestroyTexture(tex);
 	SDL_DestroyRenderer(rend);
