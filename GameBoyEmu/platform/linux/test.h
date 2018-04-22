82a5926ebf86ed063f57ab76f96572b22e6fb156
etermaw <etermaw@gmail.com>
2018-04-22 22:33:20 +0200

    fixed tests

--- GameBoyEmu/platform/linux/test.h
+++ GameBoyEmu/platform/linux/test.h
@@ -2,6 +2,7 @@
 #include "stdafx.h"
 #include "sha256.h"
 #include "core/joypad.h"
+#include "core/core.h"
 
 class Tester
 {
@@ -9,14 +10,14 @@ class Tester
 		function<void(const u32*)> render_callback; //real renderer callback
 		bool use_renderer = false;
 		bool calculate_hash = false;
-		u8* dummy_ptrs[4];
+		bool running = true;
 
 	public:
 		Tester();
-		~Tester();
 
-		bool input_stub(Joypad& input);
-
+		bool is_running() const;
+		void input_stub(Core& emu_core);
+		void render_stub(const u32* ptr);
 		void attach_renderer(function<void(const u32*)> callback);
 };
 
@@ -25,8 +26,12 @@ class Tester
 class AudioTEST
 {
 	private:
+		std::unique_ptr<u8[]> internal_buffer;
+		u8* dummy_buffers[4];
 
 	public:
+		AudioTEST();
+
 		void dummy(bool unused);
 		u8** swap_buffers(u8** ptr, u32 unused);
 
