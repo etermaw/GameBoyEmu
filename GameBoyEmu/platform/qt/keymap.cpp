a24c3df0db964a15b1d073f905bd8f7d1c6bd496
etermaw <etermaw@gmail.com>
2018-05-03 18:53:43 +0200

    changed all edits to key capture in key bindings dialog

--- GameBoyEmu/platform/qt/keymap.cpp
+++ GameBoyEmu/platform/qt/keymap.cpp
@@ -1,5 +1,3 @@
-#include <QRegularExpression>
-
 #include "keymap.h"
 #include "ui_keymap.h"
 
@@ -13,56 +11,4 @@ KeyMap::KeyMap(QWidget *parent) :
 KeyMap::~KeyMap()
 {
     delete ui;
-}
-
-void KeyMap::check_highlight_field(QKeySequenceEdit* edit)
-{
-    static const QRegularExpression field_regex("[[:alnum:]]+\\+[[:alnum:]]+"); //TODO: why making it class field introduces segfaults on exit?
-    const QString keys = edit->keySequence().toString();
-
-    if (keys.contains(field_regex))
-        edit->setStyleSheet("border: 1px solid red; border-radius: 2px;");
-
-    else
-        edit->setStyleSheet("");
-}
-
-void KeyMap::on_a_key_field_editingFinished()
-{
-    check_highlight_field(ui->a_key_field);
-}
-
-void KeyMap::on_b_key_field_editingFinished()
-{
-    check_highlight_field(ui->b_key_field);
-}
-
-void KeyMap::on_start_key_field_editingFinished()
-{
-    check_highlight_field(ui->start_key_field);
-}
-
-void KeyMap::on_select_key_field_editingFinished()
-{
-    check_highlight_field(ui->select_key_field);
-}
-
-void KeyMap::on_up_key_field_editingFinished()
-{
-    check_highlight_field(ui->up_key_field);
-}
-
-void KeyMap::on_down_key_field_editingFinished()
-{
-    check_highlight_field(ui->down_key_field);
-}
-
-void KeyMap::on_left_key_field_editingFinished()
-{
-    check_highlight_field(ui->left_key_field);
-}
-
-void KeyMap::on_right_key_field_editingFinished()
-{
-    check_highlight_field(ui->right_key_field);
 }
--- GameBoyEmu/platform/qt/keymap.h
+++ GameBoyEmu/platform/qt/keymap.h
@@ -1,7 +1,6 @@
 #ifndef KEYMAP_H
 #define KEYMAP_H
 
-#include <QKeySequenceEdit>
 #include <QDialog>
 
 namespace Ui {
@@ -16,20 +15,8 @@ class KeyMap : public QDialog
         explicit KeyMap(QWidget *parent = 0);
         ~KeyMap();
 
-    private slots:
-        void on_a_key_field_editingFinished();
-        void on_b_key_field_editingFinished();
-        void on_start_key_field_editingFinished();
-        void on_select_key_field_editingFinished();
-        void on_up_key_field_editingFinished();
-        void on_down_key_field_editingFinished();
-        void on_left_key_field_editingFinished();
-        void on_right_key_field_editingFinished();
-
     private:
         Ui::KeyMap *ui;
-
-        void check_highlight_field(QKeySequenceEdit* edit);
 };
 
 #endif // KEYMAP_H
