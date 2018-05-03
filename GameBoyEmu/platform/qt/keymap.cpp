d445d41422c8c83e5a7360e4c848af4fe6c1cdcb
etermaw <etermaw@gmail.com>
2018-05-03 16:50:23 +0200

    added wrong key (ctrl/shift/alt + whatever) highlighting

--- GameBoyEmu/platform/qt/keymap.cpp
+++ GameBoyEmu/platform/qt/keymap.cpp
@@ -3,7 +3,8 @@
 
 KeyMap::KeyMap(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::KeyMap)
+    ui(new Ui::KeyMap),
+    field_regex("[[:alnum:]]+\\+[[:alnum:]]+")
 {
     ui->setupUi(this);
 }
@@ -11,4 +12,55 @@ KeyMap::KeyMap(QWidget *parent) :
 KeyMap::~KeyMap()
 {
     delete ui;
+}
+
+void KeyMap::check_highlight_field(QKeySequenceEdit* edit)
+{
+    const QString keys = edit->keySequence().toString();
+
+    if (keys.contains(field_regex))
+        edit->setStyleSheet("border: 1px solid red; border-radius: 2px;");
+
+    else
+        edit->setStyleSheet("");
+}
+
+void KeyMap::on_a_key_field_editingFinished()
+{
+    check_highlight_field(ui->a_key_field);
+}
+
+void KeyMap::on_b_key_field_editingFinished()
+{
+    check_highlight_field(ui->b_key_field);
+}
+
+void KeyMap::on_start_key_field_editingFinished()
+{
+    check_highlight_field(ui->start_key_field);
+}
+
+void KeyMap::on_select_key_field_editingFinished()
+{
+    check_highlight_field(ui->select_key_field);
+}
+
+void KeyMap::on_up_key_field_editingFinished()
+{
+    check_highlight_field(ui->up_key_field);
+}
+
+void KeyMap::on_down_key_field_editingFinished()
+{
+    check_highlight_field(ui->down_key_field);
+}
+
+void KeyMap::on_left_key_field_editingFinished()
+{
+    check_highlight_field(ui->left_key_field);
+}
+
+void KeyMap::on_right_key_field_editingFinished()
+{
+    check_highlight_field(ui->right_key_field);
 }
--- GameBoyEmu/platform/qt/keymap.h
+++ GameBoyEmu/platform/qt/keymap.h
@@ -1,6 +1,8 @@
 #ifndef KEYMAP_H
 #define KEYMAP_H
 
+#include <QRegularExpression>
+#include <QKeySequenceEdit>
 #include <QDialog>
 
 namespace Ui {
@@ -15,8 +17,21 @@ class KeyMap : public QDialog
         explicit KeyMap(QWidget *parent = 0);
         ~KeyMap();
 
+    private slots:
+        void on_a_key_field_editingFinished();
+        void on_b_key_field_editingFinished();
+        void on_start_key_field_editingFinished();
+        void on_select_key_field_editingFinished();
+        void on_up_key_field_editingFinished();
+        void on_down_key_field_editingFinished();
+        void on_left_key_field_editingFinished();
+        void on_right_key_field_editingFinished();
+
     private:
         Ui::KeyMap *ui;
+        const QRegularExpression field_regex;
+
+        void check_highlight_field(QKeySequenceEdit* edit);
 };
 
 #endif // KEYMAP_H
