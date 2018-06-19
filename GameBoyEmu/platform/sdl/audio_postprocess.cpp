e26c84f9c88db5947d6750101f6333f197b0b234
etermaw <etermaw@gmail.com>
2018-06-20 00:36:39 +0200

    added final impl of highpass filter

--- GameBoyEmu/platform/sdl/audio_postprocess.cpp
+++ GameBoyEmu/platform/sdl/audio_postprocess.cpp
@@ -1,12 +1,6 @@
 #include "audio_postprocess.h"
 #include <SDL.h>
 
-#define NZEROS 2
-#define NPOLES 2
-#define GAIN   1.005049991e+00
-
-static float xv[NZEROS+1], yv[NPOLES+1];
-
 AudioSDL::AudioSDL()
 {
     internal_memory = std::make_unique<u8[]>(BUFFER_SIZE * 4);
@@ -38,20 +32,24 @@ u8** AudioSDL::swap_buffers(u8** buffs, u32 count)
 	float buf[2048];
     const u32 sample_count = (count + offset) / 48;
 
-	for (size_t i = 0; i < sample_count; i++)
+	for (u32 i = 0; i < sample_count; i++)
 	{
+		//mix 4 samples and normalize (audio: from -1 to 1)
         const u32 index = i * 48 + offset;
 		u8 total = buffers[0][index] + buffers[1][index] + buffers[2][index] + buffers[3][index];
+		float in = (total * (1.0f / 30.0f)) - 1.0f;
 
-		xv[0] = xv[1]; xv[1] = xv[2]; 
-        xv[2] = total * (1.0 / GAIN);
+		//apply high-pass filter
+		xv[0] = xv[1];
+		xv[1] = xv[2];
+        xv[2] = in * (1.0f / 1.005049991f);
+
         yv[0] = yv[1]; yv[1] = yv[2]; 
-        yv[2] = (xv[0] + xv[2]) - (2 * xv[1]) + (-0.9899760140 * yv[0]) + (1.9899255201 * yv[1]);
+        yv[2] = (xv[0] + xv[2]) - (2 * xv[1]) + (-0.9899760140f * yv[0]) + (1.9899255201f * yv[1]);
         float out = yv[2];
 
-
-		buf[i * 2] = out * 0.025f;
-		buf[i * 2 + 1] = out * 0.025f;
+		buf[i * 2] = out;
+		buf[i * 2 + 1] = out;
 	}
 
 	for (u32 i = 0; i < sample_count; ++i)
